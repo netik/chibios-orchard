b80bcaece4afd8ea1060ffa01b32a6fd67818aa1
John Adams <jna@retina.net>
2016-11-20 13:38:09 -0800

    add partial led code

---
+++ badge/led.c
@@ -0,0 +1,90 @@
+#include "ch.h"
+#include "hal.h"
+#include "pwm.h"
+#include "led.h"
+#include "orchard-effects.h"
+#include "gfx.h"
+
+#include "chprintf.h"
+#include "stdlib.h"
+#include "orchard-math.h"
+#include "orchard-ui.h"
+
+#include <string.h>
+#include <math.h>
+
+#define VU_T_PERIOD 2500
+#define VU_X_PERIOD 3   // number of waves across the entire band
+
+
+// hardware configuration information
+// max length is different from actual length because some
+// pattens may want to support the option of user-added LED
+// strips, whereas others will focus only on UI elements in the
+// circle provided on the board itself
+static struct led_config {
+  uint8_t       *fb; // effects frame buffer
+  uint8_t       *final_fb;  // merged ui + effects frame buffer
+  uint32_t      pixel_count;  // generated pixel length
+  uint32_t      max_pixels;   // maximal generation length
+  uint8_t       *ui_fb; // frame buffer for UI effects
+  uint32_t      ui_pixels;  // number of LEDs on the PCB itself for UI use
+} led_config;
+
+extern void ledUpdate(uint8_t *fb, uint32_t len);
+static void ledSetRGB(void *ptr, int x, uint8_t r, uint8_t g, uint8_t b, uint8_t shift);
+static void ledSetColor(void *ptr, int x, Color c, uint8_t shift);
+static void ledSetRGBClipped(void *fb, uint32_t i,
+                      uint8_t r, uint8_t g, uint8_t b, uint8_t shift);
+static Color ledGetColor(void *ptr, int x);
+
+/**
+ * @brief   Initialize Led Driver
+ * @details Initialize the Led Driver based on parameters.
+ *
+ * @param[in] leds      length of the LED chain controlled by each pin
+ * @param[out] o_fb     initialized frame buffer
+ *
+ */
+void ledStart(uint32_t leds, uint8_t *o_fb, uint32_t ui_leds, uint8_t *o_ui_fb)
+{
+  unsigned int j;
+  led_config.max_pixels = leds;
+  led_config.pixel_count = leds;
+  led_config.ui_pixels = ui_leds;
+
+  led_config.fb = o_fb;
+  led_config.ui_fb = o_ui_fb;
+
+  led_config.final_fb = chHeapAlloc( NULL, sizeof(uint8_t) * led_config.max_pixels * 3 );
+  
+  for (j = 0; j < leds * 3; j++)
+    led_config.fb[j] = 0x00;
+  for (j = 0; j < ui_leds * 3; j++)
+    led_config.ui_fb[j] = 0x00;
+
+  chSysLock();
+  ledUpdate(led_config.fb, led_config.max_pixels);
+  chSysUnlock();
+}
+
+
+static void ledSetRGBClipped(void *fb, uint32_t i,
+                      uint8_t r, uint8_t g, uint8_t b, uint8_t shift) {
+  if (i >= led_config.pixel_count)
+    return;
+  ledSetRGB(fb, i, r, g, b, shift);
+}
+
+static void ledSetRGB(void *ptr, int x, uint8_t r, uint8_t g, uint8_t b, uint8_t shift) {
+  uint8_t *buf = ((uint8_t *)ptr) + (3 * x);
+  buf[0] = g >> shift;
+  buf[1] = r >> shift;
+  buf[2] = b >> shift;
+}
+
+static void draw_pattern(void) {
+}
+
+
+
