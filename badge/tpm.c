b0d8004c031573691ad8bcc49fc12ea1598e9d58
Bill Paul <wpaul@windriver.com>
2016-11-20 15:01:10 -0800

    Add documentation to tpm module

--- badge/tpm.c
+++ badge/tpm.c
@@ -1,18 +1,82 @@
+/*-
+ * Copyright (c) 2016
+ *      Bill Paul <wpaul@windriver.com>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Bill Paul.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * This module implements support for using the KW01 TPM module as a
+ * tone generator. This simplified the generation of music notes as
+ * the hardware can be set to generate a square wave on without having
+ * to do it in software. There are two pins available for TPM output:
+ * PTC1 for TPM0 channel 0 and PTC2 for TPM0 channel 1. On the
+ * Freescale/NXP KW019032 board, PTC1 is already used for the RTC crystal,
+ * we use use TPM0 channel 1 and PTC2 here.
+ *
+ * There is a ChibiOS PWM driver for the KW01, however its API is a little
+ * clumsy to use for this application, so we use this code instead.
+ *
+ * We create a thread to play music in the background so that the game
+ * code doesn't have to block. Music is formatted as a table of PWM_NOTE
+ * structures which contain a 16-bit frequency value and a 16-bit duration
+ * value. To play a tune, the pwmThreadPlay() function is with a pointer
+ * to a tune table, and the thread will play each note in the table until
+ * it reaches a note with a duration of PWM_DURATION_END. If the last
+ * node has a duration of PWM_DURATION_LOOP, the same tune will play
+ * over and over until pwmThreadPlay(NULL) is called.
+ */
+
 #include "ch.h"
 #include "hal.h"
-#include "chschd.h"
-#include "chthreads.h"
 
 #include "tpm.h"
 #include "pwm_lld.h"
 
-#include "chprintf.h"
-extern void * stream;
-
-static THD_WORKING_AREA(waPwmThread, 64);
+static THD_WORKING_AREA(waPwmThread, 0);
 
 static PWM_NOTE * pTune;
 static thread_t * pThread;
+
+/******************************************************************************
+*
+* pwmThread - background tone generatoe thread
+*
+* This function implements a background thread for playing notes through
+* the pulse-width modulator. When music is not playing, the threads sleeps.
+* It can be woken up by pwmThreadPlay(). When wakened, pTune should point
+* to a table of PWM_NOTE structures which indicate the frequency and duration
+* for each note in the tune. The last note in the table should have a
+* duration of PWM_DURATION_END. If the duration is PWM_DURATION_LOOP, then
+* the same tune will play over and over until pwmThreadPlay(NULL) is called.
+*
+* RETURNS: N/A
+*/
 
 static THD_FUNCTION(pwmThread, arg) {
 	PWM_NOTE * p;
@@ -49,6 +113,23 @@ static THD_FUNCTION(pwmThread, arg) {
 	return;
 }
 
+/******************************************************************************
+*
+* pwmInit - initialize the PWM module and thrad
+*
+* This function intializes the TPM hardware and starts the music player
+* thread. We enable clock gating for TPM0 and set its clock source to
+* the system clock. We also program the TPM prescaler to divide the
+* system clock by a pre-defined factor (currently 8). This is done because
+* the TPM channel value field is only 16 bits wide. With a system clock of
+* 48MHz, the desired channel value for lower notes would be too large to
+* fit in 16 bits. The timer is initialized for edge-aligned PWM mode,
+* active low. The active low setting is used so that the output pin
+* goes low when no tone is playing.
+*
+* RETURNS: N/A
+*/
+ 
 void pwmInit (void)
 {
 	int i;
@@ -96,11 +177,23 @@ void pwmInit (void)
 
 	pThread = chThdCreateStatic(waPwmThread, sizeof(waPwmThread),
 		TPM_THREAD_PRIO, pwmThread, NULL);
-	/*while (pThread->p_state != CH_STATE_SLEEPING)
-		chThdSleepMilliseconds (1);*/
 
 	return;
 }
+
+/******************************************************************************
+*
+* pwmToneStart - start playing a tone
+*
+* This function programs the TPM channel for a desired tone frequency and
+* enables the timer. This produces a square wave at the desired tone on
+* the output pin, The <tone> argument is the tone frequency in Hz. The
+* tone may be changed by calling pwmToneStart() again with a different
+* frequency. The tone will continue to play until pwmToneStop() is called
+* to turn off the timer.
+*
+* RETURNS: N/A
+*/
 
 void pwmToneStart (uint32_t tone)
 {
@@ -120,6 +213,17 @@ void pwmToneStart (uint32_t tone)
 	return;
 }
 
+/******************************************************************************
+*
+* pwmToneStop - Turn off the TPM channel
+*
+* This function disables the TPM channel. If a tone is currently playing,
+* calling pwmToneStop() will cause it to stop. This function may be called
+* even if the TPM is already stopped, thought his has no effect.
+*
+* RETURNS: N/A
+*/
+
 void pwmToneStop (void)
 	{
 	/* Turn off timer and clear modulus and pulse width */
@@ -130,6 +234,40 @@ void pwmToneStop (void)
 
 	return;
 	}
+
+/******************************************************************************
+*
+* pwmThreadPlay - Play a tune using the background music thread.
+*
+* This function wakes up the background music thread to play an array of
+* notes that form a tune. The tune is formatted as an array of PWM_NOTE
+* structures. The <p> argument is a pointer to the array. Each PWM_NOTE
+* contains a frequency (pwm_note) and duration (pwm_duration). The music
+* thread will program the PWM to play the note and then pause for the
+* duration period before moving on to the next note. The following special
+* cases are handled:
+*
+* - If pwm_duration is PWM_DURATION_END, this indicates the end of the
+*   tune. The thread will go to sleep when it reaches the end of the tune
+*   and wait for pwmThreadPlay() to be called again. The value of pwm_note
+*   is ignored.
+*
+* - If pwm_duration is PWM_DURATION_LOOP, then the thread will re-play
+*   the tune over and over again. To stop the loop from playing,
+*   pwmThreadPlay() must be called with a NULL pointer.
+*
+* - If pwm_note is PWM_NOTE_PAUSE, then the TPM will be left in its
+*   current state and simply pause for the duration period.
+*
+* - If pwm_note is PWM_NOTE_NONE, then the TPM will be turned off and
+*   the thread will pause for the duration period. This can be used to
+*   create a silent pause period.
+*
+* Calling pwmThreadPlay() with a <p> pointer of NULL has no effect,
+* except to cease the playing of a tune that is looping.
+*
+* RETURNS: N/A
+*/
 
 void pwmThreadPlay (const PWM_NOTE * p)
 {
