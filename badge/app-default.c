d96dbebbddbeea92557841efb45fa6df8e8d9e5c
John Adams <jna@retina.net>
2016-11-26 15:47:06 -0800

    Clear the display when entering the badge app

--- badge/app-default.c
+++ badge/app-default.c
@@ -7,15 +7,24 @@
 #include "orchard-ui.h"
 #include <string.h>
 
+static void redraw_badge(void) {
+  // draw the entire background badge
+  gdispClear(Black);
+
+}
+
 static uint32_t default_init(OrchardAppContext *context) {
+  (void)context;
   return 0;
 }
 
 static uint32_t default_start(OrchardAppContext *context) {
-  return;
+  redraw_badge();
+  return 0;
 }
 
 void default_event(OrchardAppContext *context, const OrchardAppEvent *event) {
+
 }
 
 static void default_exit(OrchardAppContext *context) {
