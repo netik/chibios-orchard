6469eaa0be04e7a6fee14ff6c2d6ef07953728a3
Bill Paul <wpaul@windriver.com>
2016-12-16 23:41:14 -0800

    Actually change the gtimer priority

--- badge/gfxconf.h
+++ badge/gfxconf.h
@@ -210,7 +210,7 @@
 ///////////////////////////////////////////////////////////////////////////
 #define GFX_USE_GTIMER                               TRUE
 
-//#define GTIMER_THREAD_PRIORITY                       NORMAL_PRIORITY
+#define GTIMER_THREAD_PRIORITY                       NORMAL_PRIORITY
 #define GTIMER_THREAD_WORKAREA_SIZE                  0x400
 
 
