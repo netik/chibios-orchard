8b37d4198dc23ee5173ba74a30424e03635ef522
Bill Paul <wpaul@windriver.com>
2017-02-25 22:58:24 -0800

    More video hacks
    Fix the build of the videomerge program
    
    Also, modify the program to drop the first video frame. This seems to
    correct an audio/video sync issue: the video and audio track at the same
    pace, but seem to be offset slightly. Dropping the first frame seems to
    avoid this.

--- sd_card/videomerge.c
+++ sd_card/videomerge.c
@@ -4,52 +4,59 @@
 
 #define SAMPLE_CHUNKS		2
 
-#define FRAME_WIDTH             (128 * SAMPLE_CHUNKS)
-#define SAMPLES_PER_FRAME       (12 * SAMPLE_CHUNKS)
+#define FRAME_WIDTH		(128 * SAMPLE_CHUNKS)
+#define FRAME_HEIGHT		96
+#define SAMPLES_PER_FRAME	(12 * SAMPLE_CHUNKS)
 
 int
 main (int argc, char * argv[])
 {
-        FILE * audio;
-        FILE * video;
-        FILE * out;
-        uint16_t scanline[FRAME_WIDTH];
-        uint16_t samples[SAMPLES_PER_FRAME];
+	FILE * audio;
+	FILE * video;
+	FILE * out;
+	uint16_t scanline[FRAME_WIDTH];
+	uint16_t samples[SAMPLES_PER_FRAME];
+	int i;
 
-        if (argc > 4)
-                exit (1);
+	if (argc > 4)
+		exit (1);
 
-        video = fopen (argv[1], "r+");
+	video = fopen (argv[1], "r+");
 
-        if (video == NULL) {
+	if (video == NULL) {
 		fprintf (stderr, "[%s]: ", argv[1]);
-                perror ("file open failed");
-                exit (1);
-        }
+		perror ("file open failed");
+		exit (1);
+	}
 
-        audio = fopen (argv[2], "r+");
+	audio = fopen (argv[2], "r+");
 
         if (audio == NULL) {
 		fprintf (stderr, "[%s]: ", argv[2]);
-                perror ("file open failed");
-                exit (1);
-        }
+		perror ("file open failed");
+		exit (1);
+	}
 
-        out = fopen (argv[3], "w");
+	out = fopen (argv[3], "w");
 
-        if (out == NULL) {
+	if (out == NULL) {
 		fprintf (stderr, "[%s]: ", argv[3]);
-                perror ("file open failed");
-                exit (1);
-        }
+		perror ("file open failed");
+		exit (1);
+	}
 
-        while (1) {
-                if (fread (scanline, sizeof(scanline), 1, video) == 0)
-                        break;
-                if (fread (samples, sizeof(samples), 1, audio) == 0)
-                        break;
-                fwrite (scanline, sizeof(scanline), 1, out);
-                fwrite (samples, sizeof(samples), 1, out);
+	/* Hack: skip the first frame. */
+
+	for (i = 0; i < FRAME_HEIGHT; i++)
+		fread (scanline, sizeof(scanline), 1, video);
+
+	while (1) {
+		if (fread (scanline, sizeof(scanline), 1, video) == 0)
+			break;
+		if (fread (samples, sizeof(samples), 1, audio) == 0)
+			break;
+		fwrite (scanline, sizeof(scanline), 1, out);
+		fwrite (samples, sizeof(samples), 1, out);
 	}
 
 	fclose (video);
