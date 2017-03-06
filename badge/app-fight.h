29b4523f0eb2b7afda88d054f8dba3ddc2b35a01
John Adams <jna@retina.net>
2017-03-06 00:03:53 -0800

    turn off a shitton of debugging and make sure we put the ugfx button defaults back after level-up

--- badge/app-fight.h
+++ badge/app-fight.h
@@ -2,9 +2,9 @@
 #define __APP_FIGHT_H__
 
 /* debugging - this protocol can be a real pain in the ass */
-#undef  DEBUG_FIGHT_TICK        // show the clock tick events, ugfx events, and other misc events
-#define DEBUG_FIGHT_NETWORK    // show all network (radio) traffic
-#define DEBUG_FIGHT_STATE      // debug state changes
+#undef DEBUG_FIGHT_TICK        // show the clock tick events, ugfx events, and other misc events
+#undef DEBUG_FIGHT_NETWORK    // show all network (radio) traffic
+#undef DEBUG_FIGHT_STATE      // debug state changes
   
 // production use 66666 uS = 15 FPS. Eeeviil...
 // testing use 1000000 (1 sec)
