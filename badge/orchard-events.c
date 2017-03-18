d353b997cebb9e916d78b138d93677180455b7be
Bill Paul <wpaul@windriver.com>
2017-03-17 21:40:50 -0700

    Add event handling for joypad and tilt sensor
    Interrupt/event handling for the up/down/left/right joypad buttons has
    been added, as well as support for the tilt sensor.
    
    To enable them, uncomment the ENABLE_JOYPAD and ENABLE_TILT_SENSOR options
    in the Makefile.
    
    This are off by default because: a) the Freescale/NXP reference board
    doesn't have them and it has the pins wired differently and will freak
    out when the interrupts are enabled, and b) the tilt sensor seems to
    behave very badly on the prototype board. It may need some software
    de-bouncing before it is useful. Right now it's just annoying.

--- badge/orchard-events.c
+++ badge/orchard-events.c
@@ -11,36 +11,126 @@
 extern void * stream;
 
 event_source_t rf_pkt_rdy;
-event_source_t tilt_rdy;
+event_source_t joy_rdy;
+
+/* Default state is all buttons pressed. */
+
+uint8_t joyState = 0xFF;
 
 static void
-tiltInterrupt (EXTDriver *extp, expchannel_t channel)
+joyInterrupt (EXTDriver *extp, expchannel_t channel)
 {
 	(void)extp;
 	(void)channel;
 
 	chSysLockFromISR ();
-	chEvtBroadcastI (&tilt_rdy);
+	chEvtBroadcastI (&joy_rdy);
 	chSysUnlockFromISR ();
 
         return;
 }
 
 static void
-tiltIntrHandle (eventid_t id)
+joyIntrHandle (eventid_t id)
 {
 	uint8_t pad;
 
 	(void)id;
 
-	pad = palReadPad (GPIOA, 4);
+	pad = palReadPad (BUTTON_ENTER_PORT, BUTTON_ENTER_PIN);
 
-	if (pad == 1) {
-		playDoh ();
-		chprintf (stream, "Released!\r\n", pad);
-	} else if (pad == 0) {
-		playWoohoo ();
-		chprintf (stream, "Pressed!\r\n", pad);
+	pad <<= JOY_ENTER_SHIFT;
+
+	if (pad ^ (joyState & JOY_ENTER)) {
+		joyState &= ~JOY_ENTER;
+		joyState |= pad;
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Enter released!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Enter pressed!\r\n", pad);
+		}
+	}
+
+	pad = palReadPad (BUTTON_UP_PORT, BUTTON_UP_PIN);
+
+	pad <<= JOY_UP_SHIFT;
+
+	if (pad ^ (joyState & JOY_UP)) {
+		joyState &= ~JOY_UP;
+		joyState |= pad;
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Up released!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Up pressed!\r\n", pad);
+		}
+	}
+
+	pad = palReadPad (BUTTON_DOWN_PORT, BUTTON_DOWN_PIN);
+
+	pad <<= JOY_DOWN_SHIFT;
+
+	if (pad ^ (joyState & JOY_DOWN)) {
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Down released!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Down pressed!\r\n", pad);
+		}
+		joyState &= ~JOY_DOWN;
+		joyState |= pad;
+	}
+
+	pad = palReadPad (BUTTON_LEFT_PORT, BUTTON_LEFT_PIN);
+
+	pad <<= JOY_LEFT_SHIFT;
+
+	if (pad ^ (joyState & JOY_LEFT)) {
+		joyState &= ~JOY_LEFT;
+		joyState |= pad;
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Left released!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Left pressed!\r\n", pad);
+		}
+	}
+
+	pad = palReadPad (BUTTON_RIGHT_PORT, BUTTON_RIGHT_PIN);
+
+	pad <<= JOY_RIGHT_SHIFT;
+
+	if (pad ^ (joyState & JOY_RIGHT)) {
+		joyState &= ~JOY_RIGHT;
+		joyState |= pad;
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Right released!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Right pressed!\r\n", pad);
+		}
+	}
+
+	pad = palReadPad (TILT_SENSOR_PORT, TILT_SENSOR_PIN);
+
+	pad <<= JOY_TILT_SHIFT;
+
+	if (pad ^ (joyState & JOY_TILT)) {
+		joyState &= ~JOY_TILT;
+		joyState |= pad;
+		if (pad) {
+			playDoh ();
+			chprintf (stream, "Rightside up!\r\n", pad);
+		} else {
+			playWoohoo ();
+			chprintf (stream, "Upside down!\r\n", pad);
+		}
 	}
 
 	return;
@@ -56,16 +146,26 @@ tiltIntrHandle (eventid_t id)
 static const EXTConfig ext_config = {
   {
     {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART, radioInterrupt, PORTC, 3},
-    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, tiltInterrupt, PORTA, 4},
+
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTA, 4},
+#ifdef ENABLE_JOYPAD
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTA, 19},
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTC, 1},
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTC, 2},
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTC, 4},
+#endif
+#ifdef ENABLE_TILT_SENSOR
+    {EXT_CH_MODE_BOTH_EDGES | EXT_CH_MODE_AUTOSTART, joyInterrupt, PORTD, 6},
+#endif
   }
 };
 
 void orchardEventsStart(void) {
 
   chEvtObjectInit(&rf_pkt_rdy);
-  chEvtObjectInit(&tilt_rdy);
+  chEvtObjectInit(&joy_rdy);
 
   extStart(&EXTD1, &ext_config);
 
-  evtTableHook (orchard_events, tilt_rdy, tiltIntrHandle);
+  evtTableHook (orchard_events, joy_rdy, joyIntrHandle);
 }
--- badge/orchard-events.h
+++ badge/orchard-events.h
@@ -169,4 +169,22 @@ typedef struct _OrchardAppEvent {
   } ;
 } OrchardAppEvent;
 
+/* Joypad events */
+
+extern uint8_t joyState;
+
+#define JOY_ENTER	0x01
+#define JOY_UP		0x02
+#define JOY_DOWN	0x04
+#define JOY_LEFT	0x08
+#define JOY_RIGHT	0x10
+#define JOY_TILT	0x20
+
+#define JOY_ENTER_SHIFT	0
+#define JOY_UP_SHIFT	1
+#define JOY_DOWN_SHIFT	2
+#define JOY_LEFT_SHIFT	3
+#define JOY_RIGHT_SHIFT	4
+#define JOY_TILT_SHIFT	5
+
 #endif /* __ORCHARD_EVENTS__ */
