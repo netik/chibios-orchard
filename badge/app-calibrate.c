b711ab7b2d7cdeb3d2ec7eb2067e64291908a459
Bill Paul <wpaul@windriver.com>
2017-04-03 18:40:55 -0700

    Add forgotton calibrate app source

---
+++ badge/app-calibrate.c
@@ -0,0 +1,67 @@
+#include "orchard-app.h"
+#include "orchard-ui.h"
+
+#include "src/gdriver/gdriver.h"
+#include "src/ginput/ginput_driver_mouse.h"
+
+#include <string.h>
+
+static uint32_t
+calibrate_init (OrchardAppContext *context)
+{
+	(void)context;
+
+	/*
+	 * We don't want any extra stack space allocated for us.
+	 * We'll use the heap.
+	 */
+
+	return (0);
+}
+
+static void
+calibrate_start (OrchardAppContext *context)
+{
+	(void)context;
+
+	(void)ginputCalibrateMouse (0);
+
+	orchardAppExit ();
+
+	return;
+}
+
+static void
+calibrate_event (OrchardAppContext *context,
+	const OrchardAppEvent *event)
+{
+
+	(void)event;
+	(void)context;
+
+	return;
+}
+
+static void
+calibrate_exit (OrchardAppContext *context)
+{
+	userconfig * config;
+	GMouse * m;
+
+	(void)context;
+
+	m = (GMouse*)gdriverGetInstance (GDRIVER_TYPE_MOUSE, 0);
+
+	config = getConfig ();
+
+	memcpy (&config->touch_data, &m->caldata,
+	    sizeof(config->touch_data));
+	config->touch_data_present = 1;
+
+	configSave (config);
+
+	return;
+}
+
+orchard_app("Calibrate Touch Screen", 0,
+    calibrate_init, calibrate_start, calibrate_event, calibrate_exit);
