7c9aecdc46cea1a5645074f2d237419dd84c6912
Bill Paul <wpaul@windriver.com>
2017-04-05 00:45:45 -0700

    Add a LED sign app
    This is based on the scroll_lld code. The user can enter a short message
    which will be scrolled across the screen using an LED font. Tapping the
    screen exits.
    
    The font is pre-rendered as RGB files which are loaded one by one to
    match each character in the string. This allows the app itself to be
    as small as possible to preserve flash.

---
+++ badge/app-ledsign.c
@@ -0,0 +1,129 @@
+/*-
+ * Copyright (c) 2017
+ *      Bill Paul <wpaul@windriver.com>.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Bill Paul.
+ * 4. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Bill Paul AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Bill Paul OR THE VOICES IN HIS HEAD
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "orchard-app.h"
+#include "orchard-ui.h"
+
+#include "scroll_lld.h"
+
+#include <string.h>
+
+#define LEDSIGN_MAXLEN 64
+
+static uint32_t
+ledsign_init(OrchardAppContext *context)
+{
+	(void)context;
+
+	return (0);
+}
+
+static void
+ledsign_start(OrchardAppContext *context)
+{
+	OrchardUiContext * keyboardUiContext;
+
+	context->priv = chHeapAlloc (NULL, LEDSIGN_MAXLEN);
+	memset (context->priv, 0, LEDSIGN_MAXLEN);
+
+	keyboardUiContext = chHeapAlloc(NULL, sizeof(OrchardUiContext));
+	keyboardUiContext->itemlist = (const char **)chHeapAlloc(NULL,
+		sizeof(char *) * 2);
+	keyboardUiContext->itemlist[0] =
+		"Type a message,\npress ENTER when done.";
+	keyboardUiContext->itemlist[1] = context->priv;
+	keyboardUiContext->total = LEDSIGN_MAXLEN - 1;
+
+	context->instance->ui = getUiByName("keyboard");
+	context->instance->uicontext = keyboardUiContext;
+	context->instance->ui->start (context);
+
+	return;
+}
+
+static void
+ledsign_event(OrchardAppContext *context, const OrchardAppEvent *event)
+{
+	uint8_t i;
+	const OrchardUi * keyboardUi;
+	OrchardUiContext * keyboardUiContext;
+	const char * str;
+	char fname[48];
+	int sts;
+
+	keyboardUi = context->instance->ui;
+	keyboardUiContext = context->instance->uicontext;
+
+	if (event->type == ugfxEvent)
+		keyboardUi->event (context, event);
+
+ 	if (event->type == uiEvent) {
+		if (event->ui.code == uiComplete &&
+		    event->ui.flags == uiOK) {
+			keyboardUi->exit (context);
+			gdispClear (Black);
+			orchardAppExit ();
+		}
+	}
+
+ 	if (event->type == appEvent && event->app.event == appTerminate) {
+		scrollAreaSet (0, 0);
+		str = keyboardUiContext->itemlist[1];
+		do {
+			for (i = 0; i < strlen (str); i++) {
+				chsnprintf (fname, 47,
+				    "font/led/%02X.rgb", str[i]);
+				sts = scrollImage (fname, 10);
+				if (sts != 0)
+					break;
+			}
+		} while (sts == 0);
+		gdispClear (Black);
+		scrollCount (0);
+		return;
+	}
+
+	return;
+}
+
+static void
+ledsign_exit(OrchardAppContext *context)
+{
+	chHeapFree (context->priv);
+	chHeapFree (context->instance->uicontext->itemlist);
+	chHeapFree (context->instance->uicontext);
+  
+	return;
+}
+
+orchard_app("Scrolling LED sign", 0, ledsign_init, ledsign_start,
+		ledsign_event, ledsign_exit);
