0c6d209b6b25f0640de4fc404a64f637c9e41d39
John Adams <jna@retina.net>
2017-05-30 23:41:43 -0700

    Show temperature regardless of rtc state Clean up some comments

--- badge/app-default.c
+++ badge/app-default.c
@@ -368,31 +368,30 @@ static void default_event(OrchardAppContext *context,
 
   /* timed events (heal, caesar election, etc.) */
   if (event->type == timerEvent) {
-    /* draw the clock */
+    lasttemp = radioTemperatureGet (radioDriver);
+    // we're going to display this in farenheit, I don't care about yo' celsius.
+    lasttemp = (1.8 * lasttemp) + 32;
+
     if (rtc != 0) {
-      
-      // only update the temperature every ten seconds, because reading the temperature shuts down the radio receiver for 100mS
-      if ((dt.Second % 10 == 0) || (lasttemp == 0)) { 
-        lasttemp = radioTemperatureGet (radioDriver);
-        // we're going to display this in farenheit, I don't care about yo' celsius.
-        lasttemp = (1.8 * lasttemp) + 32;
-      }
-      
+      /* draw the clock */
       breakTime(rtc + ST2S((chVTGetSystemTime() - rtc_set_at)), &dt);
       chsnprintf(tmp, sizeof(tmp), "%02d:%02d:%02d | %d F", dt.Hour, dt.Minute, dt.Second, lasttemp);
-
-      gdispFillArea( totalwidth - 100, totalheight - 50,
-                     100, gdispGetFontMetric(p->fontXS, fontHeight),
-                     Black );
-      
-      gdispDrawStringBox (0,
-                          totalheight - 50,
-                          totalwidth,
-                          gdispGetFontMetric(p->fontXS, fontHeight),
-                          tmp,
-                          p->fontXS, White, justifyRight);
+    } else {
+      /* temp only */
+      chsnprintf(tmp, sizeof(tmp), "%d F", lasttemp);
     }
-
+    
+    gdispFillArea( totalwidth - 100, totalheight - 50,
+                   100, gdispGetFontMetric(p->fontXS, fontHeight),
+                   Black );
+    
+    gdispDrawStringBox (0,
+                        totalheight - 50,
+                        totalwidth,
+                        gdispGetFontMetric(p->fontXS, fontHeight),
+                        tmp,
+                        p->fontXS, White, justifyRight);
+  
     /* draw class time remaining if any */
     if (char_reset_at != 0) {
       systime_t delta = ST2S((char_reset_at - chVTGetSystemTime()));
--- badge/app-fight.c
+++ badge/app-fight.c
@@ -4,7 +4,7 @@
  * John Adams <jna@retina.net>
  * 2017
  *
- * This code implements the roman fighting game. It relies on
+ * This code implements the Roman fighting game. It relies on
  * app-default to handle healing and some game events, as well as
  * proto.c to transmit game data over the radio network.
  *
@@ -2444,6 +2444,7 @@ static void fight_recv_callback(KW01_PKT *pkt)
 
   default:
     // this shouldn't fire, but log it if it does.
+#ifdef DEBUG_FIGHT_NETWORK
 #ifdef DEBUG_FIGHT_STATE
     chprintf(stream, "%08x %d --> RECV %x - no handler (mystate=%d %s)\r\n",
              pkt->kw01_hdr.kw01_src, proto->prot_seq, u.opcode, 
@@ -2452,6 +2453,7 @@ static void fight_recv_callback(KW01_PKT *pkt)
     chprintf(stream, "%08x %d --> RECV %x - no handler (mystate=%d)\r\n",
              pkt->kw01_hdr.kw01_src, proto->prot_seq, u.opcode, 
              current_fight_state);
+#endif
 #endif
   }
 }
